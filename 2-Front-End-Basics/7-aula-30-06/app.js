99b413a98532aa9b5b19b2ec6bcb7176e8ff359a
jhyago <jaquesantunes@protonmail.com>
2025-06-30 19:59:36 -0300

    Desafio da Aula do dia 30

--- 2-Front-End-Basics/7-aula-30-06/app.js
+++ 2-Front-End-Basics/7-aula-30-06/app.js
@@ -60,3 +60,11 @@ btnLimpar.addEventListener('click', () => {
         renderizarLista()
     }
 })
+
+
+// Funcionalidades:
+
+// Marcar como comprado - Salvar esse Estado no localStorage
+// Contador de Itens - Mostrar quantos itens tem na lista, atualizando em tempo real
+// Adicione filtros para itens 'comprados' e 'pedentes'
+// Permita ordenar alfabeticamente ou por status
